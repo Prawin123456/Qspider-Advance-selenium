b5ab09f45c3c58c2adb9520e77dc9f70d3262ec1
DELL <DELL@192.168.1.16>
2025-07-29 17:46:19 +0530

    2nd commit

--- AdvanceSelenium/src/test/java/lead/LeadTest.java
+++ AdvanceSelenium/src/test/java/lead/LeadTest.java
@@ -2,6 +2,7 @@ package lead;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 import vTiger.GenericUtility.BaseClass;
 import vTiger.ObjectRepository.CreateLead;
